a7b3e2f4db6d48e75301842745fb7ee1f3f86e95
Fabily <96070580+Fabily@users.noreply.github.com>
2022-03-17 00:20:15 +0100

    Class Selector

--- ClassSelector2/Program.cs
+++ ClassSelector2/Program.cs
@@ -7,49 +7,44 @@ namespace ClassSelector
     {
         static void Main()
         {
-            SelectClasses();
+            SelectClass();
             //Somehow make a system where you can interact and choose what class you want to create and play as 
             //Create enemys and make the player able to fight them
             //Create a skill menu / Skill tree with EXP
             //Create a shop and make gear upgradable 
         }
 
-        public static void SelectClasses()
+        public static void SelectClass()
         {
+            Console.Clear();
             Console.WriteLine("Pick Your Class!");
-            string answer = Console.ReadLine();
-            string answerbutuppercase = answer.ToUpper();
-
-            while (answerbutuppercase != "Wizard" | answerbutuppercase != "Assasin" | answerbutuppercase != "Gunslinger" | answerbutuppercase != "Paladin")
+            while (true)
             {
-                if (answerbutuppercase == "WIZARD")
+                string pickclass = Console.ReadLine();
+                string classbutfoolproof = pickclass.ToUpper();
+                if (classbutfoolproof == "WIZARD")
                 {
-                    Console.WriteLine("You have choosen the class Wizard!");
-                    //Console.ReadLine();
+                    Console.WriteLine("You have chosen the class Wizard!");
                     break;
                 }
-                else if (answerbutuppercase == "ASSASSIN")
+                else if (classbutfoolproof == "ASSASSIN")
                 {
-                    Console.WriteLine("You have choose the class Assasin!");
-                    //Console.ReadLine();
+                    Console.WriteLine("You have chosen the class Assassin!");
                     break;
                 }
-                else if (answerbutuppercase == "GUNSLINGER")
+                else if (classbutfoolproof == "GUNSLINGER")
                 {
-                    Console.WriteLine("You have choose the class Gunslinger!");
-                    //Console.ReadLine();
+                    Console.WriteLine("You have chosen the class Gunslinger!");
                     break;
                 }
-                else if (answerbutuppercase == "PALADIN")
+                else if (classbutfoolproof == "PALADIN")
                 {
-                    Console.WriteLine("You have choose the class Paladin!");
-                    //Console.ReadLine();
+                    Console.WriteLine("You have chosen the class Paladin!");
                     break;
                 }
                 else
                 {
                     Console.WriteLine("Your input does not match one of the given options!");
-                    answer = Console.ReadLine();
                 }
             }
         }
